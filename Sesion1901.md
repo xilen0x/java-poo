3911437512598c9632c58e88b2ba1067838c14a0
Oriol Boix Anfosso <orboan@pluralcamp.com>
2023-01-19 13:03:46 +0000

    session 190123

---
+++ Sesion1901.md
@@ -0,0 +1,19 @@
+# POO 
+
+## Generics y Collections (continuación)
+
+- Finalizaremos el ejercicio del Carrito de la compra (Triple):
+
+https://github.com/orboan-java-nb-programming-exercises/ep-cat-java-nb-generics-collections-01/blob/main/exercici1.ipynb
+
+- Revisar el LabVehicles
+
+- Finalizaremos el ejercicio del ``Pair``.
+
+https://github.com/orboan-java-nb-programming-exercises/ep-cat-java-nb-generics-collections-00.git
+
+
+## Slides:
+
+https://github.com/orboan-daw/m032/blob/main/uf5/Collections.pdf
+
--- eclipse-projects/CarritoCompra/src/com/pluralcamp/classes/App.java
+++ eclipse-projects/CarritoCompra/src/com/pluralcamp/classes/App.java
@@ -3,10 +3,26 @@ package com.pluralcamp.classes;
 public class App {
 	public static void main(String[] args) {
 		
-		Product p1 = new Product(Category.BOOKS, "ISBN 453890K");
-		p1.setName("Java para todos");
-		p1.setPrice(4.5f);
+		//Ejercicio de Collections y Generics
 		
-		System.out.println(p1.toString());
+		Product libroJava = new Product(Category.BOOKS, "890K");
+		libroJava.setName("Java para todos");
+		libroJava.setPrice(4.5f);
+		
+		Product portatil = new Product(Category.COMPUTERS, "123");
+		portatil.setName("Vant Agile 15");
+		portatil.setPrice(1235.0f);
+		
+		Product banana = new Product(Category.FOOD, "P65");
+		banana.setName("banana");
+		banana.setPrice(0.18f);
+		
+		Cart miCarrito = new Cart();
+		
+		miCarrito.addProduct(libroJava,8);
+		miCarrito.addProduct(portatil);
+		miCarrito.addProduct(banana,20);
+		
+		miCarrito.printCart();
 	}
 }
